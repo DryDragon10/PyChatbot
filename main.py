29b851db9224e2bf31700e655358e6e5272f6fe7
Kinjal Kavish Boro <85004952+KeviTheDuck@users.noreply.github.com>
2021-08-28 14:27:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
-#imports
 import discord
 from discord.ext import commands
 from prsaw import RandomStuff
 
 
 api_key = str(input("Api key: "))
-#intro
+channel_id = int(input("Channel id: "))
+bot_token = str(input("BOT token: "))
+
 bot = commands.Bot(command_prefix="C")
-rs = RandomStuff(async_mode =True, api_key=api_key)
+rs = RandomStuff(async_mode=True, api_key = api_key)
 
 
-#inputs
 
-channel_id = int(input("ID of the channel u want the bot to talk:"))
-TOKEN = str(input("Bot TOKEN:"))
+def merge_dicts(l):
+  if len(l) == 1:
+    return l[0]
+  return {**l[0], **merge_dicts(l[1:])}
 
-
-#when bot is online
 @bot.event
 async def on_ready():
     print("Chat BOT is online")
 
-#main 
+ 
 @bot.event
 async def on_message(message):
-	if bot.user == message.author:
-		return
+    if bot.user == message.author:
+        return
 
-	if message.channel.id == channel_id:
-	 	response = await rs.get_ai_response(message.content)
-	 	await message.reply(response)
-	 	await bot.process.commands(message)
-	 	
+    if message.channel.id ==channel_id:
 
-bot.run(TOKEN)
+         r = await rs.get_ai_response(message.content)
+         response = merge_dicts(r)
+         await message.reply(response['message'])
+         await bot.process.commands(message)
+         
+
+bot.run(bot_token)
+)
